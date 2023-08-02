54026ef248979e3399c8344555ea0bb2a67fd409
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-08-02 18:59:43 +0200

    Modernizando o código parte 03

---
+++ views/paginas/niveis.php
@@ -0,0 +1,11 @@
+<div class="content-wrapper" style="min-height: 717px;">
+    <div class="content-header">
+        <div class="content-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Administrador Nível</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/paginas/usuarios.php
@@ -0,0 +1,43 @@
+<div class="content-wrapper" style="min-height: 717px;">
+    <section class="content-header">
+        <div class="content-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Administrador Usuário</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-info card-outline">
+                        <div class="card-header">
+                            <button type="button" class="btn btn-success criar-perfil" data-toggle="modal" data-target="#model-criar-perfil">
+                                Criar Novo Usuário
+                            </button><br>
+                        </div><br>
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped dt-responsive tabelaperfil" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px;">#</th>
+                                        <th>Nome</th>
+                                        <th>Usuário</th>
+                                        <th>Foto</th>
+                                        <th>Nível</th>
+                                        <th>Acções</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
